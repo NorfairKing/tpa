fa0c4e4cf1f8ea70e4020ca479d1e36332b75784
Tom Sydney Kerckhove <syd@cs-syd.eu>
2024-06-04 17:06:57 +0200

    bleh

--- tpa/default.nix
+++ tpa/default.nix
@@ -1,5 +1,5 @@
-{ mkDerivation, base, base32, bytestring, cryptonite, directory
-, filepath, lib, optparse-applicative, path, path-io, text, time
+{ mkDerivation, autodocodec, base, base32, bytestring, cryptonite
+, directory, filepath, lib, opt-env-conf, path, path-io, text, time
 , yaml
 }:
 mkDerivation {
@@ -9,8 +9,8 @@ mkDerivation {
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
-    base base32 bytestring cryptonite directory filepath
-    optparse-applicative path path-io text time yaml
+    autodocodec base base32 bytestring cryptonite directory filepath
+    opt-env-conf path path-io text time yaml
   ];
   executableHaskellDepends = [ base ];
   homepage = "https://github.com/NorfairKing/tpa#readme";
--- tpa/src/TPA/OptParse.hs
+++ tpa/src/TPA/OptParse.hs
@@ -1,24 +1,48 @@
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE TypeApplications #-}
 
-module TPA.OptParse where
+module TPA.OptParse
+  ( Settings (..),
+    getSettings,
+  )
+where
 
 import Control.Applicative
+import Control.Monad
 import Data.Text (Text)
-import Data.Yaml as Yaml
-import Options.Applicative as OptParse
+import Data.Yaml as Yaml (decodeFileThrow)
+import OptEnvConf
 import Path
 import Path.IO hiding (doesFileExist)
 import System.Directory (doesDirectoryExist, doesFileExist)
 import TPA.Key
 
 getSettings :: IO Settings
-getSettings = do
-  flags <- getFlags
-  config <- getConfiguration flags
-  combineToSettings flags config
+getSettings = runParser $ do
+  setFilter <-
+    optional $
+      strArgument
+        [ metavar "QUERY",
+          help "Query for the name of the keys to show"
+        ]
+  flagPaths <-
+    many
+      ( strOption
+          [ long "path",
+            help "Path to key files, either files or directories",
+            metavar "PATH"
+          ]
+      ) ::
+      Parser [FilePath]
+  configPaths <- confVal "keys-paths" :: Parser [FilePath]
+  setKeys <-
+    mapIO
+      (mapM (resolveFile' >=> (Yaml.decodeFileThrow . fromAbsFile)))
+      (pure (flagPaths ++ configPaths) :: Parser [FilePath])
+  -- setKeys <- pure []
+  -- let setKeys = []
+  pure Settings {..}
 
 -- | A product type for the settings that your program will use
 data Settings = Settings
@@ -26,18 +50,18 @@ data Settings = Settings
     setKeys :: ![Key]
   }
 
--- | Combine everything to 'Settings'
-combineToSettings :: Flags -> Maybe Configuration -> IO Settings
-combineToSettings Flags {..} mConf = do
-  let setFilter = flagFilter
-  setKeys <-
-    resolveKeys $
-      concat
-        [ flagPaths,
-          maybe [] configKeyPaths mConf
-        ]
-  pure Settings {..}
-
+-- -- | Combine everything to 'Settings'
+-- combineToSettings :: Flags -> Maybe Configuration -> IO Settings
+-- combineToSettings Flags {..} mConf = do
+--   let setFilter = flagFilter
+--   setKeys <-
+--     resolveKeys $
+--       concat
+--         [ flagPaths,
+--           maybe [] configKeyPaths mConf
+--         ]
+--   pure Settings {..}
+--
 resolveKeys :: [FilePath] -> IO [Key]
 resolveKeys = fmap concat . mapM go
   where
@@ -59,85 +83,86 @@ resolveKeys = fmap concat . mapM go
     goFile :: Path Abs File -> IO [Key]
     goFile = Yaml.decodeFileThrow . fromAbsFile
 
-data Configuration = Configuration
-  { configKeyPaths :: ![FilePath]
-  }
-
-instance FromJSON Configuration where
-  parseJSON = withObject "Configuration" $ \o ->
-    Configuration
-      <$> o .:? "key-paths" .!= []
-
--- | Get the configuration
-getConfiguration :: Flags -> IO (Maybe Configuration)
-getConfiguration Flags {..} =
-  case flagConfigFile of
-    Nothing -> defaultConfigFile >>= forgivingAbsence . decodeFileThrow . fromAbsFile
-    Just cf -> do
-      afp <- resolveFile' cf
-      Just <$> decodeFileThrow (fromAbsFile afp)
-
--- | Where to get the configuration file by default.
 --
--- This uses the XDG base directory specifictation:
--- https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
-defaultConfigFile :: IO (Path Abs File)
-defaultConfigFile = do
-  xdgConfigDir <- getXdgDir XdgConfig (Just [reldir|tpa|])
-  resolveFile xdgConfigDir "config.yaml"
-
--- | Get the command-line flags
-getFlags :: IO Flags
-getFlags = customExecParser prefs_ flagsParser
-
--- | The 'optparse-applicative' parsing preferences
-prefs_ :: OptParse.ParserPrefs
-prefs_ =
-  OptParse.defaultPrefs
-    { OptParse.prefShowHelpOnError = True,
-      OptParse.prefShowHelpOnEmpty = True
-    }
-
--- | The @optparse-applicative@ parser for 'Flags'
-flagsParser :: OptParse.ParserInfo Flags
-flagsParser =
-  OptParse.info
-    (OptParse.helper <*> parseFlags)
-    OptParse.fullDesc
-
--- | The flags that are common across commands.
-data Flags = Flags
-  { flagConfigFile :: !(Maybe FilePath),
-    flagFilter :: !(Maybe Text),
-    flagPaths :: ![FilePath]
-  }
-
--- | The 'optparse-applicative' parser for the 'Flags'.
-parseFlags :: OptParse.Parser Flags
-parseFlags =
-  Flags
-    <$> optional
-      ( strOption
-          ( mconcat
-              [ long "config-file",
-                help "Path to an altenative config file",
-                metavar "FILEPATH"
-              ]
-          )
-      )
-    <*> optional
-      ( strArgument
-          ( mconcat
-              [ help "the key to show"
-              ]
-          )
-      )
-    <*> many
-      ( strOption
-          ( mconcat
-              [ long "path",
-                help "Path to key files, either files or directories",
-                metavar "PATH"
-              ]
-          )
-      )
+-- data Configuration = Configuration
+--   { configKeyPaths :: ![FilePath]
+--   }
+--
+-- instance FromJSON Configuration where
+--   parseJSON = withObject "Configuration" $ \o ->
+--     Configuration
+--       <$> o .:? "key-paths" .!= []
+--
+-- -- | Get the configuration
+-- getConfiguration :: Flags -> IO (Maybe Configuration)
+-- getConfiguration Flags {..} =
+--   case flagConfigFile of
+--     Nothing -> defaultConfigFile >>= forgivingAbsence . decodeFileThrow . fromAbsFile
+--     Just cf -> do
+--       afp <- resolveFile' cf
+--       Just <$> decodeFileThrow (fromAbsFile afp)
+--
+-- -- | Where to get the configuration file by default.
+-- --
+-- -- This uses the XDG base directory specifictation:
+-- -- https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+-- defaultConfigFile :: IO (Path Abs File)
+-- defaultConfigFile = do
+--   xdgConfigDir <- getXdgDir XdgConfig (Just [reldir|tpa|])
+--   resolveFile xdgConfigDir "config.yaml"
+--
+-- -- | Get the command-line flags
+-- getFlags :: IO Flags
+-- getFlags = customExecParser prefs_ flagsParser
+--
+-- -- | The 'optparse-applicative' parsing preferences
+-- prefs_ :: OptParse.ParserPrefs
+-- prefs_ =
+--   OptParse.defaultPrefs
+--     { OptParse.prefShowHelpOnError = True,
+--       OptParse.prefShowHelpOnEmpty = True
+--     }
+--
+-- -- | The @optparse-applicative@ parser for 'Flags'
+-- flagsParser :: OptParse.ParserInfo Flags
+-- flagsParser =
+--   OptParse.info
+--     (OptParse.helper <*> parseFlags)
+--     OptParse.fullDesc
+--
+-- -- | The flags that are common across commands.
+-- data Flags = Flags
+--   { flagConfigFile :: !(Maybe FilePath),
+--     flagFilter :: !(Maybe Text),
+--     flagPaths :: ![FilePath]
+--   }
+--
+-- -- | The 'optparse-applicative' parser for the 'Flags'.
+-- parseFlags :: OptParse.Parser Flags
+-- parseFlags =
+--   Flags
+--     <$> optional
+--       ( strOption
+--           ( mconcat
+--               [ long "config-file",
+--                 help "Path to an altenative config file",
+--                 metavar "FILEPATH"
+--               ]
+--           )
+--       )
+--     <*> optional
+--       ( strArgument
+--           ( mconcat
+--               [ help "the key to show"
+--               ]
+--           )
+--       )
+--     <*> many
+--       ( strOption
+--           ( mconcat
+--               [ long "path",
+--                 help "Path to key files, either files or directories",
+--                 metavar "PATH"
+--               ]
+--           )
+--       )
